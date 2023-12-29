3838643a75509742a8be71ea754397a2356b88aa
Andrew L. Weekes <alweekes@users.noreply.github.com>
2023-12-29 08:39:07 +0000

    Add files via upload

---
+++ Pi_NTP_Display.py
@@ -0,0 +1,335 @@
+#!/usr/bin/python
+#--------------------------------------
+#
+#   Pi NTP Server Stats + Clock Display
+#   LCD Driver with text justification
+#
+#   Displaytech 204A 20x4 LCD
+#   https://uk.rs-online.com/web/p/lcd-monochrome-displays/5326818
+#
+#--------------------------------------
+
+# The wiring for the LCD is as follows:
+# 1 : GND [RPi Pin 6]
+# 2 : 5V [RPi Pin 4]
+# 3 : Contrast (0-5V) [10K pot between RPi Pins 4,6 wiper to LCD]
+# 4 : RS (Register Select) [RPi Pin 26]
+# 5 : R/W (Read Write) [0V via link on LCD]
+# 6 : Enable or Strobe [RPi Pin 24]
+# 7 : Data Bit 0 [N.C.]
+# 8 : Data Bit 1 [N.C.]
+# 9 : Data Bit 2 [N.C.]
+# 10: Data Bit 3 [N.C.]
+# 11: Data Bit 4 [RPi Pin 22]
+# 12: Data Bit 5 [RPi Pin 18]
+# 13: Data Bit 6 [RPi Pin 16]
+# 14: Data Bit 7 [RPi Pin 15]
+# 15: LCD Backlight +5V**
+# 16: LCD Backlight GND
+
+# The wiring for the GPS is as follows:
+# Main NMEA via USB (/dev/ttyAMA0)
+# PPS signal for accurate timing GPIO Pin 18
+
+#import
+import os
+import pynmea2
+import RPi.GPIO as GPIO
+import time
+from datetime import datetime
+import subprocess
+from gps import *
+import re
+
+# Define GPIO to LCD mapping
+LCD_RS = 7
+LCD_E  = 8
+LCD_D4 = 25
+LCD_D5 = 24
+LCD_D6 = 23
+LCD_D7 = 22
+
+# Define some device constants
+LCD_WIDTH = 20    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
+LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+def main():
+  # Main program block
+  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+
+  # Initialise display
+  lcd_init()
+
+  # Send some centred test
+  lcd_string("--------------------",LCD_LINE_1,2)
+  lcd_string("Rasbperry Pi 1",LCD_LINE_2,2)
+  lcd_string("Model B",LCD_LINE_3,2)
+  lcd_string("--------------------",LCD_LINE_4,2)
+  time.sleep(0.5) # 3 second delay
+  lcd_string("GPS Disciplined",LCD_LINE_1,2)
+  lcd_string("NTP Time Server",LCD_LINE_2,2)
+  lcd_string("v1.0 28/12/23",LCD_LINE_3,2)
+  lcd_string("Andrew L. Weekes",LCD_LINE_4,2)
+  time.sleep(0.5) # 20 second delay
+
+  #Time to display each stats page (seconds)
+  stats_delay=2
+  #Time to display time page (seconds)
+  time_cycles=10
+
+  while True:
+
+#    displayTime(time_cycles)
+#    displayChronyStats(stats_delay)
+    displayGPSdata()
+
+def displayTime(cycles):
+  # Blank display
+  lcd_byte(0x01, LCD_CMD)
+  # Initialise cycl count
+  x=1
+
+  # Print page title
+  lcd_string("Date and Time",LCD_LINE_1,2)
+  lcd_string("--------------------",LCD_LINE_2,2)
+
+ #Initialise time and date stringa
+  timestr = ""
+  datestr = ""
+
+  while x < cycles:
+    #Get current date and time
+    now = datetime.now()
+
+    #Update only if changed
+    if timestr != now.strftime("%H:%M:%S"):
+      timestr = now.strftime("%H:%M:%S")
+      os.system('clear')
+      print(timestr)
+      lcd_string(timestr,LCD_LINE_3,2)
+
+    if datestr != now.strftime("%b %d, %Y"):
+      datestr = now.strftime("%b %d, %Y")
+      os.system('clear')
+      print(datestr)
+      lcd_string(datestr,LCD_LINE_4,2)
+
+    x += 1
+    time.sleep(1)
+
+def displayGPSData():
+  # Blank display
+  lcd_byte(0x01, LCD_CMD)
+
+  # Get output of gpspipe, output is native GPSD JSON sentences + NMEA messages
+  # -r = raw NMEA senteces, -x 2 causes gpspipe to close after 2 second
+  try:
+    output = subprocess.check_output("gpspipe -r -x 2", shell=True, text=True)
+    returncode = 0
+  except subprocess.CalledProcessError as e:
+    output = e.output
+    returncode = e.returncode
+    print (returncode)
+    exit(1)
+
+  #Split output into list elements
+  gpsPipe = output.split()
+
+  # Pattern to match, keep NMEA messages and discard JSON list items
+  pattern = "$GP"
+  filtered_gpsPipe = [x for x in gpsPipe if x.startswith(pattern)]
+  print (filtered_gpsPipe)
+
+  gpsdata = pynmea2.parse(filtered_gpsPipe)
+  print (gpsdata)
+
+# Debugging to display list elements for above choices
+#  for count, item in enumerate(gpsResult):
+#    print (count, item)
+
+def displayChronyStats():
+  # Blank display
+  lcd_byte(0x01, LCD_CMD)
+
+  try:
+    output = subprocess.check_output("chronyc tracking", shell=True, text=True)
+    returncode = 0
+  except subprocess.CalledProcessError as e:
+    output = e.output
+    returncode = e.returncode
+    print (returncode)
+    exit(1)
+
+  #Split chronyc output into element list
+  chronyResult = output.split()
+
+  #Output stats to console and LCD
+
+  #Page 1
+  os.system('clear')
+  print("System Time: ")
+  print(chronyResult[20] + " " + "s " + chronyResult[22])
+  print("Last offset: ")
+  print(chronyResult[29] + " s")
+  lcd_string("System Time: ",LCD_LINE_1,1)
+  lcd_string(chronyResult[20] + " " + "s " + chronyResult[22],LCD_LINE_2,1)
+  lcd_string("Last offset: ",LCD_LINE_3,1)
+  lcd_string(chronyResult[29] + " s",LCD_LINE_4,1)
+  time.sleep(delay)
+
+  #Page 2
+  os.system('clear')
+  print("RMS offset: ")
+  print(chronyResult[34] + " s")
+  print("Frequency: ")
+  print(chronyResult[38] + " " + chronyResult[39] + " " + chronyResult[40])
+  lcd_string("RMS offset: ",LCD_LINE_1,1)
+  lcd_string(chronyResult[34] + " s",LCD_LINE_2,1)
+  lcd_string("Frequency: ",LCD_LINE_3,1)
+  lcd_string(chronyResult[38] + " " + chronyResult[39] + " " + chronyResult[40],LCD_LINE_4,1)
+  time.sleep(delay)
+
+  #Page 3
+  os.system('clear')
+  print("Residual frequency: ")
+  print(chronyResult[44] + " " + chronyResult[45])
+  print("Skew: ")
+  print(chronyResult[48] + " " + chronyResult[49])
+  lcd_string("Residual frequency: ",LCD_LINE_1,1)
+  lcd_string(chronyResult[44] + " " + chronyResult[45],LCD_LINE_2,1)
+  lcd_string("Skew: ",LCD_LINE_3,1)
+  lcd_string(chronyResult[48] + " " + chronyResult[49],LCD_LINE_4,1)
+  time.sleep(delay)
+
+  #Page 4
+  os.system('clear')
+  print("Root delay: ")
+  print(chronyResult[53] + " sec")
+  print("Root dispersion: ")
+  print(chronyResult[58] + " sec")
+  lcd_string("Root delay: ",LCD_LINE_1,1)
+  lcd_string(chronyResult[53] + " sec",LCD_LINE_2,1)
+  lcd_string("Root dispersion: ",LCD_LINE_3,1)
+  lcd_string(chronyResult[58] + " sec",LCD_LINE_4,1)
+  time.sleep(delay)
+
+  #Page 5
+  os.system('clear')
+  print("Update Interval:")
+  print(chronyResult[63] + " sec")
+  print("Leap Status: ")
+  print(chronyResult[68])
+  lcd_string("Update Interval:",LCD_LINE_1,1)
+  lcd_string(chronyResult[63] + " sec",LCD_LINE_2,1)
+  lcd_string("Leap Status: ",LCD_LINE_3,1)
+  lcd_string(chronyResult[68],LCD_LINE_4,1)
+  time.sleep(delay)
+
+# Debugging to display list elements for above choices
+#  for count, item in enumerate(chronyResult):
+#  print (count, item)
+
+
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = data
+  # mode = True  for character
+  #        False for command
+  GPIO.output(LCD_RS, mode) # RS
+
+  # High bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x10==0x10:
+    GPIO.output(LCD_D4, True)
+  if bits&0x20==0x20:
+    GPIO.output(LCD_D5, True)
+  if bits&0x40==0x40:
+    GPIO.output(LCD_D6, True)
+  if bits&0x80==0x80:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+  # Low bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x01==0x01:
+    GPIO.output(LCD_D4, True)
+  if bits&0x02==0x02:
+    GPIO.output(LCD_D5, True)
+  if bits&0x04==0x04:
+    GPIO.output(LCD_D6, True)
+  if bits&0x08==0x08:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+def lcd_toggle_enable():
+  # Toggle enable
+  time.sleep(E_DELAY)
+  GPIO.output(LCD_E, True)
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)
+  time.sleep(E_DELAY)
+
+def lcd_string(message,line,style):
+  # Send string to display
+  # style=1 Left justified
+  # style=2 Centred
+  # style=3 Right justified
+
+  if style==1:
+    message = message.ljust(LCD_WIDTH," ")
+  elif style==2:
+    message = message.center(LCD_WIDTH," ")
+  elif style==3:
+    message = message.rjust(LCD_WIDTH," ")
+
+  lcd_byte(line, LCD_CMD)
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
+
+if __name__ == '__main__':
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    lcd_byte(0x01, LCD_CMD)
+    lcd_string("Goodbye!",LCD_LINE_1,2)
+    GPIO.cleanup()
