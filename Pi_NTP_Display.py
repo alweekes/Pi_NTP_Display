81c2d0a681840bb6eb2257d0b46027bb29b1a067
Andrew L. Weekes <alweekes@users.noreply.github.com>
2024-01-04 13:58:14 +0000

    Update Pi_NTP_Display.py

--- Pi_NTP_Display.py
+++ Pi_NTP_Display.py
@@ -155,7 +155,7 @@ def displayNetworkData(page, delay):
   #Get ip address
   try:
     output = subprocess.check_output(['hostname', '--all-ip-addresses'])
-    print (str(output).strip(b\n))
+    print (str(output).strip("b\n"))
     lcd_string("IP: " + str(output).strip("bn"),LCD_LINE_1,1)
 
   except Exception as e:
